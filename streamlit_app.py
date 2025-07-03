413485de3dbdea1d9c0c3b887e1a2597e5762074
miguel leal <117935181+Migue916@users.noreply.github.com>
2025-07-03 16:06:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
+
+    
     ingredients_string = ingredients_string.strip() # Remove trailing space
 
     my_insert_stmt = f"""
